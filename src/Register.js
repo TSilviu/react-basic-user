ab4f8fd096ec0c3a8575bd74c158a323d3a6783c
Silviu Tugulan <t.silviu94@gmail.com>
2018-04-02 22:46:45 +0100

    Add UI for Register page

--- src/Register.js
+++ src/Register.js
@@ -1,9 +1,80 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Card, CardText } from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
 export default class Register extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: {
+        name: '',
+        email: '',
+        password: ''
+      }
+    }
+
+    this.processForm = this.processForm.bind(this);
+    this.changeUser = this.changeUser.bind(this);
+  }
+
+  processForm(event) {
+    event.preventDefault();
+
+    console.log('email:', this.state.user.name);
+    console.log('password:', this.state.user.password);
+  }
+
+  changeUser(event) {
+    const field = event.target.name;
+    const user = this.state.user;
+    user[field] = event.target.value;
+
+    this.setState({user});
+  }
+
   render() {
     return(
-      <div>Register form</div>
+      <Card>
+        <form onSubmit={this.processForm}>
+          <h2>Sign Up</h2>
+
+          <div>
+            <TextField
+              floatingLabelText="Name"
+              name="name"
+              onChange={this.changeUser}
+              value={this.state.user.name}
+            />
+          </div>
+
+          <div>
+            <TextField
+              floatingLabelText="Email"
+              name="email"
+              onChange={this.changeUser}
+              value={this.state.user.email}
+            />
+          </div>
+
+          <div>
+            <TextField
+              floatingLabelText="Password"
+              type="password"
+              name="password"
+              onChange={this.changeUser}
+              value={this.state.user.password}
+            />
+          </div>
+
+          <RaisedButton type="submit" label="Register" primary />
+
+          <CardText>Already have an account? <Link to={'/login'}>Log in</Link></CardText>
+
+        </form>
+      </Card>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import App from './App';
 import './index.css';
 
+// remove tap delay, essential for MaterialUI to work properly
+injectTapEventPlugin();
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <MuiThemeProvider muiTheme={getMuiTheme()}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
