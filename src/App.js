21c621eb9fba1df9d8b7d5adafbf98ba7654ce0b
Silviu Tugulan <t.silviu94@gmail.com>
2018-04-02 21:43:27 +0100

    Add links to register and login pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { Link, Route } from 'react-router-dom';
+
+import Register from './Register.js';
+import Login from './Login.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Some div
+        <Link to="/register">Register</Link>
+        <Link to="/login">Log in</Link>
+
+        <Route path="/register" component={Register}/>
+        <Route path="/login" component={Login}/>
       </div>
     );
   }
