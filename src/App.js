a889066690673d6cb9046628d2d33d4cf7277ce6
Silviu Tugulan <t.silviu94@gmail.com>
2018-04-03 14:38:11 +0100

    Add home screen

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ import Register from './Register.js';
 import Login from './Login.js';
 import './App.css';
 
+function HomeScreen() {
+  return (
+    <div>
+      <Link to="/register">Register</Link>
+      <Link to="/login">Log in</Link>
+    </div>
+  );
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Link to="/register">Register</Link>
-        <Link to="/login">Log in</Link>
-
+        <Route exact path="/" component={HomeScreen}/>
         <Route path="/register" component={Register}/>
         <Route path="/login" component={Login}/>
       </div>
